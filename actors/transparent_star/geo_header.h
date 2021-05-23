25cfed520203350940d064bb80a5cba7d886e468
AllyTally <alexiatilde@gmail.com>
2021-05-22 22:54:46 -0300

    Fix blue star transparency

--- actors/transparent_star/geo_header.h
+++ actors/transparent_star/geo_header.h
@@ -1,7 +1,7 @@
 extern const GeoLayout transparent_star_geo[];
-extern Vtx transparent_star_blue_starout_mesh_vtx_0[46];
+extern Vtx transparent_star_blue_starout_mesh_vtx_0[44];
 extern Gfx transparent_star_blue_starout_mesh_tri_0[];
-
+extern Gfx mat_transparent_star_lambert57_material_f3d_v3_layer5[];
+extern Gfx mat_revert_transparent_star_lambert57_material_f3d_v3_layer5[];
 extern Gfx transparent_star_blue_starout_mesh[];
 extern Gfx transparent_star_material_revert_render_settings[];
-
